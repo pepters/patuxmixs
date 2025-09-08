b01a16943fed571fdc71123a5c5f5b4c244f2a97
mixay <pk020311@gmail.com>
2025-09-08 18:32:54 +0300

    fixed possible memory leak

--- src/main/java/com/mixay/patuxmix/commands/getNScommand.java
+++ src/main/java/com/mixay/patuxmix/commands/getNScommand.java
@@ -3,28 +3,27 @@ package com.mixay.patuxmix.commands;
 import com.mixay.patuxmix.util.*;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 
 import java.text.MessageFormat;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import net.jodah.expiringmap.ExpiringMap;
 
 public class getNScommand implements CommandExecutor {
-    Map <Player, Long> cooldowns = ExpiringMap.builder().expiration(5, TimeUnit.SECONDS).build();
+    Map <UUID, Long> cooldowns = ExpiringMap.builder().expiration(5, TimeUnit.SECONDS).build();
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-            if (!cooldowns.containsKey(p)) {
+            if (!cooldowns.containsKey(p.getUniqueId())) {
                 int breakradius = 1;
                 if (args.length > 0) {
                     try {
@@ -45,9 +44,9 @@ public class getNScommand implements CommandExecutor {
                     } else {
                         p.sendMessage(ChatColor.RED + "У вас нет свободного места для получения лопаты!");
                     }
-                cooldowns.put(p, System.currentTimeMillis());
+                cooldowns.put(p.getUniqueId(), System.currentTimeMillis());
             } else {
-                p.sendMessage(MessageFormat.format(ChatColor.RED + "Вам надо подождить еще {0} секунд для повторного использования команды", TimeUnit.SECONDS.convert(TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS) - (System.currentTimeMillis() - cooldowns.get(p)), TimeUnit.MILLISECONDS)));
+                p.sendMessage(MessageFormat.format(ChatColor.RED + "Вам надо подождить еще {0} секунд для повторного использования команды", TimeUnit.SECONDS.convert(TimeUnit.MILLISECONDS.convert(5, TimeUnit.SECONDS) - (System.currentTimeMillis() - cooldowns.get(p.getUniqueId())), TimeUnit.MILLISECONDS)));
             }
         } else {
             sender.sendMessage(ChatColor.RED + "This is player only command!");
